eb611ca3b8d4f30abdb4df78f41c3d563f1c4fc5
Akshat <akshatjangir1710@gmail.com>
2023-03-12 17:11:10 +0530

    remove extra

--- client/src/state/api.js
+++ client/src/state/api.js
@@ -1,22 +1,3 @@
-// import { createApi, fetchBaseQuerry } from "@reduxjs/toolkit/query/react";
-
-// export const api = createApi({
-//   baseQuery: fetchBaseQuerry({ baseUrl: import.meta.env.VITE_BASE_URL }),
-//   reducerPath: "main",
-//   tagTypes: [],
-//   endpoints: (build) => ({
-//     postAiText: build.mutation({
-//       query: (payload) => ({
-//         url: "openai/text",
-//         method: "POST",
-//         body: payload,
-//       }),
-//     }),
-//   }),
-// });
-
-// export const { usePostAiTextMutation } = api;
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
